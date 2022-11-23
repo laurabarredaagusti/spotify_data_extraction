3aa2d1149d3008b9ffd447efc7e71aa677cee7fe
lauragreemko <laura.barreda@greemko.com>
2022-11-23 17:25:52 +0530

    removed extra code

--- utils/variables.py
+++ utils/variables.py
@@ -4,10 +4,7 @@ artist_features_list = ['genres', 'popularity']   # Artist genres and artist po
 
 audio_features_list = ['danceability', 'energy', 'key', 'loudness', 'mode',  
                        'speechiness', 'acousticness', 'instrumentalness', 'liveness',   # Audio features  
-                       'valence', 'tempo', 'duration_ms', 'time_signature'] 
-
-CLIENT_ID = 'b3efc4982a5a4c7197f979d08087128d'   # client id, personal credential
-CLIENT_SECRET = '3cd6cc8bdf114d9a97be07ad12024683'   # client secret, personal credential    
+                       'valence', 'tempo', 'duration_ms', 'time_signature']  
 
 AUTH_URL = 'https://accounts.spotify.com/api/token'   # Authorisation URL
 
